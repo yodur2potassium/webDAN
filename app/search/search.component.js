cbd807126998d4bda925720032c903c41076e012
Jérémy Lejeune <jeremy.lejeune-stagiaire@laposte.fr>
2016-06-10 13:53:25 +0200

    Search component to improve

--- app/search/search.component.js
+++ app/search/search.component.js
@@ -9,14 +9,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-// import { Error } from "../error/error";
 var SearchComponent = (function () {
     function SearchComponent() {
         this.values = '';
     }
     SearchComponent.prototype.onKey = function (value) {
+        var _this = this;
         this.values = value;
-        console.log(this.values);
+        this.results = [];
+        if (this.values.length > 3) {
+            var input = '\\b' + this.values;
+            var regexp_1 = new RegExp(input, 'i');
+            var results = this.errors.map(function (error) {
+                return regexp_1.test(error.title) ? error : undefined;
+                // let res = regexp.test(error.title)
+                // console.log(res);
+            });
+            results.forEach(function (el) {
+                el ? _this.results.push(el) : null;
+            });
+            console.log(this.results);
+        }
     };
     __decorate([
         core_1.Input(), 
@@ -25,7 +38,7 @@ var SearchComponent = (function () {
     SearchComponent = __decorate([
         core_1.Component({
             selector: 'my-search',
-            template: "\n    <input #box (keyup)=\"onKey(box.value)\">\n  "
+            template: "\n    <input #box (keyup)=\"onKey(box.value)\">\n    <div>\n      <ul>\n        <li *ngFor=\"let result of results\"><h3 *ngIf=\"result\">{{ result.title }}</h3></li>\n      </ul>\n    </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], SearchComponent);
--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -1,18 +1,39 @@
 import { Component, Input } from "@angular/core";
-// import { Error } from "../error/error";
+import { Error } from "../error/error";
 
 @Component({
   selector: 'my-search',
   template: `
     <input #box (keyup)="onKey(box.value)">
+    <div>
+      <ul>
+        <li *ngFor="let result of results"><h3 *ngIf="result">{{ result.title }}</h3></li>
+      </ul>
+    </div>
   `
 })
 export class SearchComponent {
   @Input()
   errors: Error[];
   values='';
+  results: Error[];
+
   onKey(value:string) {
     this.values = value;
-    console.log(this.values)
+    this.results = []
+    if (this.values.length > 3){
+      let input = '\\b' + this.values;
+      let regexp = new RegExp(input, 'i');
+      let results = this.errors.map(error =>{
+        return regexp.test(error.title)?error:undefined;
+        // let res = regexp.test(error.title)
+        // console.log(res);
+      })
+      results.forEach(el=>{
+        el?this.results.push(el):null;
+      })
+      console.log(this.results);
+
+    }
   }
 }
